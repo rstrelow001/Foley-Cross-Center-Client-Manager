b70c8192685a2bed9cfac88ba5d17e40d9c9a568
ntawil <36651883+ntawil@users.noreply.github.com>
2018-10-04 20:12:36 -0500

    edits to viewing visit records

--- FoleyCross/visitRecords/urls.py
+++ FoleyCross/visitRecords/urls.py
@@ -3,10 +3,12 @@ from . import views
 from django.conf.urls import url
 from django.views.generic import ListView, DetailView
 from .models import Visit
+from familyRecords.models import Family
 from django.http import HttpResponseRedirect
 from django.http import HttpRequest
 
-urlpatterns = [path('', ListView.as_view(queryset=Visit.objects.all().order_by("-date")[:25], template_name="visitRecords/visitRecords.html")),
-
+urlpatterns = [path('', ListView.as_view(queryset=Family.objects.all().order_by("-date")[:25], template_name="visitRecords/visitRecords.html")),
+               path('visitsByFamily/', views.visitsByFamily, name='visitsByFamily'),
+               path('visitSummary/', views.visitSummary, name='visitSummary'),
                path('enterVisit/', views.enterVisit, name='enterVisit'),
 ]
--- FoleyCross/visitRecords/views.py
+++ FoleyCross/visitRecords/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django import forms
 from .models import VisitForm, Visit
-
+from familyRecords.models import Family
 
 def enterVisit(request):
     # if this is a POST request we need to process the form data
@@ -23,3 +23,51 @@ def enterVisit(request):
         form = VisitForm()
 
     return render(request, 'visitRecords/newVisit.html', {'form': form})
+
+
+def visitsByFamily(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = VisitForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            newForm = form.save(commit=False)
+            newForm.save()
+            return HttpResponseRedirect('..')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        family_id = request.GET.get('id', '')
+        family_name = request.GET.get('f_name', '')
+        family = Family.objects.get(pk=family_id)
+        visits = family.visit_set.all()
+    return render(request, 'visitRecords/visitsByFamily.html', {'visits': visits})
+
+
+def visitSummary(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = VisitForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            newForm = form.save(commit=False)
+            newForm.save()
+            return HttpResponseRedirect('..')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        visit_id = request.GET.get('id', '')
+        #visit = Visit.objects.get(pk=visit_id)
+        #visits = visit.visit_set.all()
+    return render(request, 'visitRecords/visitsByFamily.html', {'visits': visit_id})
+
+
+
