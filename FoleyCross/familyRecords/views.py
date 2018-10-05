fde30823ba80e03161dbb2beea293eceeccdff77
rstrelow001 <rstrelow001@csbsju.edu>
2018-10-04 20:05:41 -0500

    "further modifications to viewing, adding, and updating family records"

--- FoleyCross/familyRecords/views.py
+++ FoleyCross/familyRecords/views.py
@@ -6,7 +6,7 @@ from .models import PersonForm, FamilyForm, Person, Family
 
 
 
-def enterPerson(request):
+def newPerson(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
@@ -16,22 +16,23 @@ def enterPerson(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
+            id = request.GET.get('familyid', '')
             newForm = form.save(commit=False)
             newForm.save()
-            return HttpResponseRedirect('../..')
+            return HttpResponseRedirect('../updateFamily/?id=' + id)
 
     # if a GET (or any other method) we'll create a blank form
     else:
         '''person = Person.objects.get(pk=1)'''
-        id= request.GET.get('id', '')
+        id= request.GET.get('familyid', '')
         results = Family.objects.filter(pk=id)
         form = PersonForm(family=forms.ModelChoiceField(queryset=results.all(), initial=1))
 
-    return render(request, 'familyRecords/newPerson.html', {'form': form})
+    return render(request, 'familyRecords/personForm.html', {'form': form})
 
 
 
-def editPerson(request):
+def updatePerson(request):
     # if this is a POST request we need to process the form data
 
     if request.method == 'POST':
@@ -47,25 +48,21 @@ def editPerson(request):
             person = Person.objects.get(pk=personid)
             form = PersonForm(request.POST, instance= person)
             form.save()
-            return HttpResponseRedirect('../'+request.GET.get('familyid', ''))
+            #return to view the family this person is in
+            return HttpResponseRedirect('../updateFamily/?id='+request.GET.get('familyid', ''))
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        '''person = Person.objects.get(pk=1)'''
+        #grab the current values stored for this person
         id= request.GET.get('personid', '')
         results = Person.objects.get(pk=id)
         form = PersonForm(instance=results)
 
-    return render(request, 'familyRecords/newPerson.html', {'form': form})
+    return render(request, 'familyRecords/personForm.html', {'form': form})
 
 
 
-
-
-
-
-
-def enterFamily(request):
+def newFamily(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
@@ -83,4 +80,36 @@ def enterFamily(request):
     else:
         form = FamilyForm()
 
-    return render(request, 'familyRecords/newFamily.html', {'form': form})
+    return render(request, 'familyRecords/familyForm.html', {'form': form})
+
+
+
+def updateFamily(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = FamilyForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            family_id = request.GET.get('id', '')
+            family = Family.objects.get(pk=family_id)
+            form = FamilyForm(request.POST, instance=family)
+            newForm = form.save(commit=False)
+            newForm.save()
+            return HttpResponseRedirect('..')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        family_id = request.GET.get('id', '')
+        family=Family.objects.get(pk=family_id)
+        familyForm = FamilyForm(instance=family)
+        members= family.person_set.all()
+        forms = []
+        for person in members:
+            newPerson = PersonForm(instance=person)
+            forms.append(newPerson)
+
+    return render(request, 'familyRecords/updateFamily.html', {'form': familyForm, 'family': family, 'members': members})
